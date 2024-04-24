67f91044a0669617ea63577abbe63660248e2b6e
R4C00n <superc0605@gmail.com>
2024-04-24 23:09:29 +0700

    add distances to Individual and Population, change fitness, fix repair-problem, change local-search

--- Plot.py
+++ Plot.py
@@ -2,24 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 def Plot_solution(sensors_positions:list[list[float,float]], solution:list[list[int,float]]):
-    # x = []
-    # y = []
-    # sleep_x = []
-    # sleep_y = []
-    # radius = []
-    # for i in range(len(sensors_positions)):
-    #     if(solution[i][0]==1):
-    #         x.append(sensors_positions[i][0])
-    #         y.append(sensors_positions[i][1])
-    #         radius.append(solution[i][1])
-    #     else:
-    #         sleep_x.append(sensors_positions[i][0])
-    #         sleep_y.append(sensors_positions[i][1])
-    # plt.scatter(x,y,s=radius,c='blue')
-    # plt.scatter(sleep_x,sleep_y,c='red')
-    # plt.show()
-
-
     fix, ax = plt.subplots()
     active_x = []
     active_y = []
@@ -33,5 +15,6 @@ def Plot_solution(sensors_positions:list[list[float,float]], solution:list[list[
     ax.scatter(active_x,active_y,marker='x')
     ax.set_xlim(-10,1010)
     ax.set_ylim(-100,100)
+    ax.set_aspect('equal',adjustable='box')
     plt.show()
 
